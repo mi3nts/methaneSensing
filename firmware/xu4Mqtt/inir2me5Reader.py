fe94004ac78129225c5888358b71e5c6bcc260a3
lakihaomal <lhw150030@utdallas.edu>
2025-01-30 14:28:33 -0600

    inir2me5 finished

--- firmware/xu4Mqtt/inir2me5Reader.py
+++ firmware/xu4Mqtt/inir2me5Reader.py
@@ -26,8 +26,8 @@ def main():
         time.sleep(1)
         print("\nConnected to: " + ser.portstr + "\n")
         time.sleep(1)
-        print("Entering Configuration Mode")
 
+        print("Entering Configuration Mode")
         configMode, response   = send_command("C",ser)
         if(configMode):
             print("In Configuration Mode")
@@ -39,51 +39,58 @@ def main():
             print("Printing  Settings")
             printSettings(response,dateTime)
 
+        print("Entering Engineering Mode")
+        EngineeringMode, response   = send_command("B",ser)
+        if(EngineeringMode):
+            print("In Engineering Mode")
 
-    # lineASCII = []
+            lineASCII = []
+            startTime = time.time()
 
+            while True:
+                try:
+                    # Read bytes from the serial buffer
+                    for byte in ser.read():
+                        ascii_char = chr(byte)  # if 32 <= byte <= 126 else "."  # Handle printable and non-printable characters
+                        lineASCII.append(ascii_char)
 
-    #     while True:
-    #         try:
-    #             # Read bytes from the serial buffer
-    #             for byte in ser.read():
-    #                 ascii_char = chr(byte)  # if 32 <= byte <= 126 else "."  # Handle printable and non-printable characters
-    #                 lineASCII.append(ascii_char)
+                        data = ''.join(lineASCII)
+                        lines = data.split("\n\r")
+                        lines = [line for line in lines if line.strip()]
 
-    #                 data = ''.join(lineASCII)
-    #                 lines = data.split("\n\r")
-    #                 lines = [line for line in lines if line.strip()]
+                        # print(lines)
+                        if lines and lines[-1] == "0000005d":
+                            if lines[0] == "0000005b":
+                                dateTime = datetime.datetime.now()
+                                print(lines)
+                                print("============================")
+                                print("= Methan Sensor Data Found =")
 
-    #                 print(lines)
-    #                 if lines and lines[-1] == "0000005d":
-    #                     if lines[0] == "0000005b":
-    #                         print(lines)
-    #                         methanePPM        = int(lines[1], 16)
-    #                         faultCode         = lines[2]
-    #                         sensorTemperature = (int(lines[3], 16)/10)- 273.15
-    #                         CRC               = int(lines[4], 16)
-    #                         CRC1sComp         = int(lines[5], 16)                  
-    #                         print("Methane Concentration = " + str(methanePPM) + " ppm")
-    #                         print("Fault Code            = " + str(faultCode))
-    #                         print("Sensor Temperature    = " + str(sensorTemperature) + " C")
-    #                         print("CRC                   = " + str(CRC))
-    #                         print("CRC1sComp             = " + str(CRC1sComp))
-    #                         # send_command("A",ser)
-    #                     lineASCII = []
-    #                     lines     = []
+                                sensorDictionary = OrderedDict([
+                                    ("dateTime", str(dateTime)),
+                                    ("methane",            int(lines[1], 16)),  
+                                    ("faultCode",          int(lines[2], 16)),  
+                                    ("temperature",        (int(lines[3], 16)/10)- 273.15),  
+                                    ("ref1SecAverage",     int(lines[4], 16)), 
+                                    ("act1SecAverage",     int(lines[5], 16)), 
+                                    ("crc",                int(lines[6], 16)), 
+                                    ("crc1sComp",          int(lines[7], 16)), 
+                                            ])
+                                pprint(sensorDictionary)
+                                
+                                if time.time() - startTime> 60 :
+                                    print("Sensor Warmed Up")
+                                    mSR.sensorFinisher(dateTime,"INIR2ME5",sensorDictionary)
+                                
+                                lineASCII = []
+                                lines     = []
 
-    #                 # else:
-    #                 #     print(f"Last line is not '0000005d'. Found: {lines[-1] if lines else 'None'}")
-
-
-            
-    #         except Exception as e:
-    #             print(f"Incomplete read. Error: {e}")
-    #             break  # Exit the loop if an error occurs
+                except Exception as e:
+                    print(f"Incomplete read. Error: {e}")
+                    break  # Exit the loop if an error occurs
 
     except serial.SerialException as e:
         print(f"Failed to connect to {methanePort}. Error: {e}")
-
 
 
 def printSettings(response,dateTime):
