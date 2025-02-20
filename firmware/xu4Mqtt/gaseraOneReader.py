bc677da210c3a0adc0164cf731dd351492daba48
lakihaomal <lhw150030@utdallas.edu>
2025-02-20 16:55:01 -0600

    minor updates

--- firmware/xu4Mqtt/gaseraOneReader.py
+++ firmware/xu4Mqtt/gaseraOneReader.py
@@ -38,9 +38,10 @@ class GaseraOneSensor:
     
         
     def parse_ak_response(self, response: bytes) -> str:
+
         """Parses the AK response and extracts relevant information."""
         if response.startswith(b'\x02') and response.endswith(b'\x03'):
-            print(response)
+            # print(response)
             response_str = response[1:-1].decode()
             parts = re.findall(r'([^\s"]+|\"[^\"]*\")', response_str)
 
@@ -93,7 +94,69 @@ class GaseraOneSensor:
                 device_name = parts[2] if len(parts) > 2 else ""
                 return f"Error Status: {error_status}, Device Name: {device_name}"
         
+            elif parts[0] == "AITR":
+                error_status = parts[1]
+                iteration_number = parts[2] if len(parts) > 2 else ""
+                return f"Error Status: {error_status}, Iteration Number: {iteration_number}"
 
+            elif parts[0] == "ANET":
+                error_status = parts[1]
+                use_dhcp = parts[2]
+                ip_address = parts[3]
+                netmask = parts[4]
+                gateway = parts[5]
+                return (f"Error Status: {error_status}, Use DHCP: {use_dhcp}, IP Address: {ip_address}, "
+                        f"Netmask: {netmask}, Gateway: {gateway}")
+                            
+            elif parts[0] == "ACLK":
+                error_status = parts[1]
+                datetime_string = parts[2] if len(parts) > 2 else "Unknown"
+                return f"Error Status: {error_status}, Date/Time: {datetime_string}"
+            
+            elif parts[0] == "ATSP":
+                error_status = parts[1]
+                parameters = parts[2:] if len(parts) > 2 else []
+                return f"Error Status: {error_status}, Parameters: {parameters}"
+            
+            elif parts[0] == "ASYP":
+                error_status = parts[1]
+                parameters = [param for param in parts[2:] if param != b'NULL'] if len(parts) > 2 else []
+                parameters = [','.join([item for item in string.split(',') if item != 'NULL']) for string in parameters]
+                return f"Error Status: {error_status}, System Parameters: {parameters}"
+            
+            elif parts[0] == "AMPS":
+                error_status = parts[1]
+                parameters = parts[2:] if len(parts) > 2 else []
+                return f"Error Status: {error_status}, Multi-Point Sampler Parameters: {parameters}"
+            
+            elif parts[0] == "ADEV":
+                error_status = parts[1]
+                manufacturer = parts[2] if len(parts) > 2 else ""
+                serial_number = parts[3] if len(parts) > 3 else ""
+                device_name = parts[4] if len(parts) > 4 else ""
+                firmware_version = parts[5] if len(parts) > 5 else ""
+                return f"Error Status: {error_status}, Manufacturer: {manufacturer}, Serial Number: {serial_number}, Device Name: {device_name}, Firmware Version: {firmware_version}"
+            
+            elif parts[0] == "STST":
+                error_status = parts[1]
+                return f"Error Status: {error_status}, Self-Test Request Status: {'Success' if error_status == '0' else 'Error'}"
+            
+
+            elif parts[0] == "ASTR":
+                error_status = parts[1]
+                self_test_result = parts[2]
+                
+                # Interpret the self-test state/result
+                if error_status == "0":
+                    if self_test_result == "-2":
+                        return "Self-test result: N/A (test not started)"
+                    elif self_test_result == "-1":
+                        return "Self-test in progress"
+                    elif self_test_result == "0":
+                        return "Self-test failed"
+                    elif self_test_result == "1":
+                        return "Self-test completed successfully"
+        
         return "Invalid response format"
     
     
@@ -168,7 +231,6 @@ class GaseraOneSensor:
         self.socket.sendall(request_name)
         response_name = self.socket.recv(1024)
         return self.parse_ak_response(response_name)
-
     
     def request_iteration_number(self):
         """Request current measurement iteration number and parse the response."""
@@ -176,6 +238,49 @@ class GaseraOneSensor:
         self.socket.sendall(request_iteration)
         response_iteration = self.socket.recv(1024)
         return self.parse_ak_response(response_iteration)
+
+    def request_network_settings(self):
+        """Request current network settings and parse the response."""
+        request_network = self.format_ak_request("ANET")
+        self.socket.sendall(request_network)
+        response_network = self.socket.recv(1024)
+        return self.parse_ak_response(response_network)
+
+    def request_device_datetime(self):
+        """Request current device date and time."""
+        request_datetime = self.format_ak_request("ACLK")
+        self.socket.sendall(request_datetime)
+        response_datetime = self.socket.recv(1024)
+        return self.parse_ak_response(response_datetime)
+
+    def request_task_parameters(self, task_id: str):
+        """Request measurement task parameters."""
+        request_task = self.format_ak_request("ATSP", data=task_id)
+        self.socket.sendall(request_task)
+        response_task = self.socket.recv(1024)
+        return self.parse_ak_response(response_task)
+    
+    def request_system_parameters(self):
+        """Request system parameters."""
+        request_sys_params = self.format_ak_request("ASYP")
+        self.socket.sendall(request_sys_params)
+        time.sleep(.1)
+        response_sys_params = self.socket.recv(4096)
+        return self.parse_ak_response(response_sys_params)
+    
+    def request_multi_point_sampler(self):
+        """Request multi-point sampler parameters."""
+        request_mps = self.format_ak_request("AMPS")
+        self.socket.sendall(request_mps)
+        response_mps = self.socket.recv(1024)
+        return self.parse_ak_response(response_mps)
+
+    def request_device_info(self):
+        """Request device information and parse the response."""
+        request_info = self.format_ak_request("ADEV")
+        self.socket.sendall(request_info)
+        response_info = self.socket.recv(1024)
+        return self.parse_ak_response(response_info)
 
 
     def get_device_status(self):
@@ -198,10 +303,30 @@ class GaseraOneSensor:
         """Retrieves the last measurement results (concentrations)."""
         return self.request_last_measurement_results()
 
-
     def get_measurement_status(self):
         """Retrieves the measurement status."""
         return self.request_measurement_status()
+
+
+    def start_self_test(self):
+        """Request to start the device self-test routine."""
+        request_test = self.format_ak_request("STST")
+        self.socket.sendall(request_test)
+        response_test = self.socket.recv(1024)
+        time.sleep(.1)
+        return self.parse_ak_response(response_test)
+
+
+    def request_device_self_test_result(self):
+        """Request the result of the device self-test."""
+        request_test = self.format_ak_request("ASTR")
+        self.socket.sendall(request_test)
+        response_test = self.socket.recv(1024)
+        return self.parse_ak_response(response_test)
+
+    def get_device_self_test_result(self):
+        """Retrieves the device self-test result."""
+        return self.request_device_self_test_result()
 
 if __name__ == "__main__":
     # Create an instance of the sensor
@@ -217,26 +342,44 @@ if __name__ == "__main__":
     print(f"Measurement Status: {sensor.get_measurement_status()}")
     print(f"Device Name: {sensor.request_device_name()}")    
     print(f"Iteration Number: {sensor.request_iteration_number()}")
+    print(f"Network Settings: {sensor.request_network_settings()}")
+    print(f"Device Date/Time: {sensor.request_device_datetime()}")
+    print(f"Multi-Point Sampler Parameters: {sensor.request_multi_point_sampler()}")
+    print(f"System Parameters: {sensor.request_system_parameters()}")
+    print(f"Measurement Task Parameters: {sensor.request_task_parameters('11')}")
+    print(f"Device Info: {sensor.request_device_info()}")
+    print(f"Self-Test Status: {sensor.start_self_test()}")
+    time.sleep(120)
+    print(f"Device Self-Test Result: {sensor.get_device_self_test_result()}")
+    time.sleep(120)
+    print(f"Stop Measurement: {sensor.stop_measurement()}")
+    time.sleep(120)
+    task_id = "11"  # Example task ID
+    print(f"Start Measurement Response: {sensor.request_start_measurement(task_id)}")
+    time.sleep(120)
     
-    # time.sleep(60)
-    # print(f"Stop Measurement: {sensor.stop_measurement()}")
-    # time.sleep(60)
-    # task_id = "11"  # Example task ID
-    # print(f"Start Measurement Response: {sensor.request_start_measurement(task_id)}")
-    # time.sleep(60)
+    print(f"Last Measurement Results:\n{sensor.get_last_measurement_results()}")
+    time.sleep(30)
+
+    print(f"Last Measurement Results:\n{sensor.get_last_measurement_results()}")
+    time.sleep(30)
+
+    print(f"Last Measurement Results:\n{sensor.get_last_measurement_results()}")
+    time.sleep(30)
     
-    # print(f"Last Measurement Results:\n{sensor.get_last_measurement_results()}")
-    # time.sleep(60)
+    print(f"Last Measurement Results:\n{sensor.get_last_measurement_results()}")
+    time.sleep(30)
 
-    # print(f"Last Measurement Results:\n{sensor.get_last_measurement_results()}")
-    # time.sleep(60)
+    print(f"Last Measurement Results:\n{sensor.get_last_measurement_results()}")
+    time.sleep(30)
 
-    # print(f"Last Measurement Results:\n{sensor.get_last_measurement_results()}")
-    # time.sleep(60)
+    print(f"Last Measurement Results:\n{sensor.get_last_measurement_results()}")
+    time.sleep(30)
 
-    
+    print(f"Last Measurement Results:\n{sensor.get_last_measurement_results()}")
     time.sleep(1)
-    # print(f"Stop Measurement: {sensor.stop_measurement()}")
+
+    print(f"Stop Measurement: {sensor.stop_measurement()}")
     
     # Disconnect when done
     sensor.disconnect()
